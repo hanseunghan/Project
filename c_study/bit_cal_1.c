47650af1e5b932f32bf5fda90a9afcfa87231bca
hanseunghan <32289542+hanseunghan@users.noreply.github.com>
2024-11-13 16:43:49 +0900

    241113_2

--- c_study/bit_cal_1.c
+++ c_study/bit_cal_1.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 
 #define BIT_SET(    index, x)   ((x)->data[index>>5] |= 1<<(index&31))
+#define BIT_ISSET(  index, x)   ((x)->data[index>>5] & (1<<index&31))
+#define BIT_CLR(    index, x)   ((x)->data[index>>5] &= -(1<<(index&31)))
+#define BIT_ZERO(x) do{ int i;      \
+                            for(i=0; i<32; i++) \
+                                (x)->data[i] = 0}while(0)
 
+typedef struct
+{
+    int data[32];
+    int count;
+}bit_set;
 
 int main()
 {
+    bit_set item;
+    int i;
 
+    BIT_ZERO(   &item   );
+    BIT_SET(    700, &item  );
+    BIT_SET(    800, &item  );
+
+    for(i=0; i<1024; i++)
+        if( BIT_ISSET(  i, item))
+            printf("%d\n", i);
+
+    return 0;
 }
